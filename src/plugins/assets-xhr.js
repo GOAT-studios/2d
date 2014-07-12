7848c658d3652334f6fc8875f78d16d748dba808
Tuur Dutoit <tuur.dutoit@live.nl>
2014-07-12 18:33:55 +0200

    Implemented assets-xhr plugin

--- src/plugins/assets-xhr.js
+++ src/plugins/assets-xhr.js
@@ -5,103 +5,157 @@ var Assets;
 
 
 Assets = function() {
-	this.assets = [];
+    this.assets = [];
 
-	this.loading = 0;
-	this.loaded = 0;
-	this.errors = 0;
-	this.total = 0;
+    this.loading = 0;
+    this.success = 0;
+    this.errors  = 0;
+    this.total   = 0;
 
-	return this;
+    this.loadedSize = 0;
+    this.errorSize  = 0;
+    this.totalSize  = 0;
+
+    return this;
 }
 
-Assets.prototype.load = function(url, name) {
-	var elem = this.getElement(url);
-	this.setEvents(elem);
-	this.loading++;
-	this.total++;
+Assets.prototype.name = "assets-xhr";
+Assets.prototype.type = "Assets";
 
-	this.assets.push({
-		name: name,
-		url: url,
-		element: elem
-	});
+Assets.prototype.load = function(url, name, forceText) {
+    if(typeof name === "boolean") {forceText = name; name = undefined;}
+    var assets = this;
 
-	return this;
+//Update stats
+    assets.loading++;
+    assets.total++;
+
+//Create an Assets object
+    var obj = new Asset(url, name);
+
+// Create an XHR request
+    var xhr = obj.xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+
+    if(!forceText) {
+        xhr.responseType = "arraybuffer";
+    }
+
+    xhr.onreadystatechange = function(e) {
+        //Get some info from headers
+        if(xhr.readyState === 2) {
+            obj.totalSize = xhr.getResponseHeader("Content-Length");
+            obj.mime = xhr.getResponseHeader("Content-Type");
+        }
+
+        //Asset loaded/errored
+        if(xhr.readyState === 4) {
+            assets.loading--;
+            if(xhr.status === 200) {
+                assets.success++;
+                obj.done  = true;
+                obj.error = false;
+
+                if(forceText) {
+                    obj.data = xhr.responseText || xhr.response;
+                }
+                else {
+                    obj.data  = xhr.response;
+                    
+                    //Generate objectURL
+                    var arrBuffView = new Uint8Array(obj.data);
+                    var blob = new Blob([arrBuffView], {type: obj.mime});
+                    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+                    obj.blobURL = url;
+                }
+            }
+            else {
+                assets.errors++;
+                obj.done  = true;
+                obj.error = true;
+                obj.data  = null;
+
+                obj.loadedSize = 0;
+                obj.totalSize  = 0;
+            }
+        }
+    }
+
+    //Catch Progress event
+    xhr.onprogress = function(e) {
+        if(xhr.status === 200) {
+            obj.loadedSize = e.loaded;
+            obj.totalSize  = e.total || e.totalSize;
+
+            assets.updateProgress();
+        }
+    }
+
+    xhr.send();
+
+// Push the objects to the assets list
+    this.assets.push(obj);
+
+    return this;
 }
 
 Assets.prototype.get = function(id) {
-	var assets = this.assets;
-	for(var i = 0, len = assets.length; i < len; i++) {
-		var asset = assets[i];
-		if(asset.name === id || asset.url === id) {
-			return asset.element;
-		}
-	}
+    var assets = this.assets;
 
-	return null;
+    if(id) {
+        for(var i = 0, len = assets.length; i < len; i++) {
+            var asset = assets[i];
+            if(asset.name === id || asset.url === id) {
+                return asset;
+            }
+        }
+    }
+    else {
+        // No ID: return last added asset
+        return assets[assets.length - 1];
+    }
+
+    return null;
 }
 
-Assets.prototype.getElement = function(url) {
-	var ext = this.getExtension(url);
-	var elem;
+Assets.prototype.updateProgress = function() {
+//Private
+//Update sizes (called on each progress event)
+    this.totalSize  = 0;
+    this.loadedSize = 0;
+    this.errorSize  = 0;
 
-	switch(ext) {
-		case "jpg","jpeg","png","gif":
-			elem = document.createElement("img");
-			elem.setAttribute("src", url);
-			elem.setAttribute("type", this.mimes[ext]);
-			break;
-		case "mp3","wav","ogg":
-			elem = document.createElement("audio");
-			var source = document.createElement("source");
-			source.setAttribute("src", url);
-			source.setAttribute("type", this.mimes[ext]);
-			elem.appendChild(source);
-			break;
-		case "mp4","m4v","webm":
-			elem = document.createElement("video");
-			var source = document.createElement("source");
-			source.setAttribute("src", url);
-			source.setAttribute("type", this.mimes[ext]);
-			elem.appendChild(source);
-			break;
-		default:
-			elem = document.createElement("script");
-			elem.setAttribute("src", "url");
-			elem.setAttribute("type", this.mimes[ext]);
-	}
-
-	return elem;
-}
-
-Assets.prototype.getExtension = function(url) {
-	var split = url.split(".");
-	return split[split.length - 1].substr(1).toLowerCase();
-}
-
-Assets.prototype.setEvents = function(elem) {
-	var assets = this;
-	elem.onload = function(e) {
-		assets.loaded++;
-		assets.loading--;
-
-		assets.emitProgress();
-		assets.emit("load", e);
-		if(assets.loading === 0) assets.emit("done");
-	}
-	elem.onerror = function(e) {
-		assets.errors++;
-		assets.loading--;
-
-		assets.emitProgress();
-		assets.emit("error", e);
-		if(assets.loading === 0) asset.emit("done");
-	}
-
-	return this;
+    for(var i = 0, len = this.assets.length; i < len; i++) {
+        var asset = this.assets[i];
+        this.totalSize += asset.totalSize || 0;
+        if(asset.error) {
+            this.errorSize += asset.totalSize || 0;
+        }
+        else {
+            this.loadedSize += asset.loadedSize || 0;
+        }
+    }
 }
 
 
 
-})
+
+
+var Asset = function(url, name) {
+    this.name = name || null;
+    this.url = url || null;
+    this.mime = null;
+    this.done = false; // Whether the XHR has finished loading (errors included)
+    this.error = null; // Whether the XHR has errored
+    this.loadedSize = 0;
+    this.totalSize = 0;
+    this.xhr = null;
+    this.data = null;
+    this.blobURL = null;
+
+    return this;
+}
+
+
+
+})()
