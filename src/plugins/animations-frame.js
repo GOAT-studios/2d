6f8742f006aeccb6d9fc7745510bd296cfa37680
Tuur Dutoit <tuur.dutoit@live.nl>
2014-07-19 18:19:44 +0200

    Added Sprites and Animations plugins

---
+++ src/plugins/animations-frame.js
@@ -0,0 +1,71 @@
+var Animations;
+
+(function() {
+
+
+
+Animations = function() {
+	return this;
+}
+
+Animations.prototype.name = "animations-frame";
+Animations.prototype.type = "Animation";
+
+Animations.prototype.Init = function(game) {
+	this.game = game;
+}
+
+
+
+
+/*
+ * Animation
+ *
+ * Represents an animation (frame by frame)
+ */
+
+var Animation = function(name, frames, speed) {
+	this.name = name;
+	this.speed = speed;
+	this.frames = [];
+	this.pauseDuration = 0;
+
+	for(var i = 0, len = frames.length; i < len; i++) {
+		if(frames[i].sheet && frames[i].position && frames[i].dimensions) {
+			this.frames.push(frames[i]);
+		}
+		else {
+			this.frames.push(Animation.game.Sprite(frames[i]));
+		}
+	}
+
+	this.startTime = Game.prototype.Utils.time();
+
+	return this;
+}
+
+Animation.prototype.update = function() {
+	var currTime = Game.prototype.Utils.time();
+	var index = (currTime - this.startTime - this.pauseDuration) % this.speed;
+	this.currentFrame = this.frames[index];
+
+	return this;
+}
+
+Animation.prototype.pause = function() {
+	this.pauseTime = Game.prototype.Utils.time();
+	return this;
+}
+
+Animation.prototype.start = function() {
+	this.lastStartTime = Game.prototype.Utils.time();
+	this.pauseDuration += this.lastStartTime - this.pauseTime;
+
+	return this;
+}
+
+Animations.prototype.Animation = Animation;
+
+
+
+})();
---
+++ src/plugins/sprites-basic.js
@@ -0,0 +1,22 @@
+var Sprite;
+
+(function() {
+
+
+
+Sprite = function(spriteSheet, pos, dim) {
+	this.sheet = spriteSheet;
+	this.position = pos;
+	this.dimensions = dim;
+	
+	return this;
+}
+
+Sprite.prototype.name = "sprites-basic";
+Sprite.prototype.type = "Sprites";
+
+
+
+
+
+})();
