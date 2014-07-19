c6f9e93e2f8a3eac7805ebb53f54eb2749a4a0af
Tuur Dutoit <tuur.dutoit@live.nl>
2014-07-19 17:34:49 +0200

    Began sample save files

--- test/sampleWorld.js
+++ test/sampleWorld.js
@@ -1,100 +1,55 @@
-var SampleWorld = {
-	name: "sample",
-	type: "sidescroller",
-	Blocks: {
-		background: function(d) { //Object = function that draw itself
-			d.drawImage(d.getAsset("background.png"), 0, 0, d.getCanvasWidth(), d.getCanvasHeight());
-		},
-		earth: function(d) {
-			d.drawImage(d.getAsset("earth.jpg"), 0, 0, d.getWidth(), d.getHeight());
-		},
-		earthTop: function(d) {
-			d.drawImage(d.getAsset("earth-top.jpg"), 0, 0, d.getWidth(), d.getHeight());
-		},
-		empty: function() {},
-		door: function(d) {
-			d.fillStyle("#242424").fillRect(-3, (d.getHeight()/2)+1, 4, 2);
-			d.fillStyle("#663300").fillRect(0, 5, 1, (d.getHeight()/2)-10)
-			                      .fillRect(0, (d.getHeight()/2)+5, 1, (d.getHeight()/2)-10)
-			                      .fillRect(1, 0, d.getWidth()-1, d.getHeight());
-		}
-	},
-	Levels: [ //World->levels
-		[ //Level->categories
-			{ //Category->Objects
-				"name": "backgrounds",
-				"Objects": [
-					"background"
-				]
-			},
+var SampleWorld = [
+	{
+		name: "level1",
+		index: 0,
+		scenes: [
 			{
-				"name": "blocks",
-				"Objects": [
-					["earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth"],
-					["earth","earth","earth","empty","empty","empty","empty","earth"],
-					["earth","earth","earth","empty","empty","empty","empty","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth","earth"],
-					["earth","earth","earth","empty","empty","empty","earth"],
-					["earth","earth","earth","empty","empty","empty","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth","empty","empty","empty","earth"],
-					["earth","earth","earth","empty","empty","empty","earth"],
-					["earth","earth","earth"],
-					["earth","earth","earth","empty","empty","empty","empty","earth"],
-					["earth","earth","earth","empty","empty","empty","empty","earth","earth"],
-					["earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth","earth"]
-				]
-			},
-			{
-				"name": "objects",
-				"Objects": [
+				name: "level1",
+				Backgrounds: [
 					{
-						name: "door",
-						position: {
-							x: 25*35-5,
-							y: 3*35
+						position: {x:0, y:0},
+						dimensions: {h:3523, w:346332534634345254243},
+						parallax: {x:0.75, y:1},
+						Update: function(game) {
+							this.position.x = game.Camera.position.x * this.parallax.x;
+							this.position.y = game.Camera.position.y * this.parallax.y;
 						},
-						width: 10,
-						height: 2*35,
-						Draw: "door"
-					},
-					{
-						"name": "door",
-						position: {
-							x: 25*35-5,
-							y: 9*35
-						},
-						width: 10,
-						height: 2*35,
-						Draw: "door"
+						Draw: function(d) {
+							d.drawImage(d.getAsset("background1"), this.position.x, this.position.y);
+						}
 					}
-				]
-			},
-			{
-				name: "player",
-				life: 100,
-				money: 0,
-				items: {
-					sword: 1,
-					chestPlate: 1,
-					bread: 4,
-					wood: 0,
-					gun: 0
+				],
+				Terrain: [
+					{
+						position: {x:0, y:0},
+						type: "earth",
+						Draw: function(d) {
+							d.drawImage(d.getAsset("sprite-earth"), 0, 0);
+						}
+					}
+				],
+				Objects: [
+					{
+						position: {x:0, y:0},
+						dimensions: {h:234, w:32},
+						Init: function(game) {
+							game.Behaviours.TwoPoint.register(this, {x:0, y:0}, {x:25, y:40}, 2);
+						}
+						Update: function(game) {
+							game.Behaviours.TwoPoint.update(this);
+						}
+					}
+				],
+				Foregrounds: [
+
+				],
+				SpawnPoints: [
+
+				],
+				Music: {
+
 				}
 			}
 		]
-	]
-}
+	}
+]
