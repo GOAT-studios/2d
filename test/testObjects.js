7b0df6df7c975141157de9c195837f81bc4fdebe
Tuur Dutoit <tuur.dutoit@live.nl>
2014-07-23 13:32:22 +0200

    Converted some tabs to spaces

--- test/testObjects.js
+++ test/testObjects.js
@@ -5,10 +5,10 @@ var Objects = {
         position: {x:0, y:0},
         dimensions: {h:234, w:32},
         Init: function(game) {
-			game.Behaviours.TwoPoint.register(this, {x:0, y:0}, {x:25, y:40}, 2);
+            game.Behaviours.TwoPoint.register(this, {x:0, y:0}, {x:25, y:40}, 2);
         },
         Update: function(game) {
             game.Behaviours.TwoPoint.update(this);
-		}
+        }
     }
 }
