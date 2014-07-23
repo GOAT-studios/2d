b03dc91eb1d410c8f5ae53b6caa5df0f56cd5ea8
Tuur Dutoit <tuur.dutoit@live.nl>
2014-07-23 12:56:21 +0200

    Beginnings of World plugin

--- src/plugins/world-sidescroller.js
+++ src/plugins/world-sidescroller.js
@@ -5,13 +5,31 @@ var World;
 
 
 World = function() {
-	return this;
+    return this;
 }
 
 World.prototype.name = "world-sidescroller";
 World.prototype.type = "World";
 
+World.prototype.Init = function(game) {
+    this.game = game;
+}
 
+World.prototype.load = function(world) {
+    this.blocks = world.blocks;
+    this.scenes = world.scenes;
+
+    return this;
+}
+
+World.prototype.loadScene = function(name, spawnPoint) {
+    var mother = this.scenes[name];
+    var scene = mother[this.game.Save.loadedFile.states[name]];
+    var this.currentScene = this.parseScene(scene);
+
+    this.game.Player.spawn(this.currentScene.SpawnPoints[spawnPoint]);
+    if()
+}
 
 
 
--- test/sampleWorld.js
+++ test/sampleWorld.js
@@ -1,55 +1,68 @@
-var SampleWorld = [
-	{
-		name: "level1",
-		index: 0,
-		scenes: [
-			{
-				name: "level1",
-				Backgrounds: [
-					{
-						position: {x:0, y:0},
-						dimensions: {h:3523, w:346332534634345254243},
-						parallax: {x:0.75, y:1},
-						Update: function(game) {
-							this.position.x = game.Camera.position.x * this.parallax.x;
-							this.position.y = game.Camera.position.y * this.parallax.y;
-						},
-						Draw: function(d) {
-							d.drawImage(d.getAsset("background1"), this.position.x, this.position.y);
-						}
-					}
-				],
-				Terrain: [
-					{
-						position: {x:0, y:0},
-						type: "earth",
-						Draw: function(d) {
-							d.drawImage(d.getAsset("sprite-earth"), 0, 0);
-						}
-					}
-				],
-				Objects: [
-					{
-						position: {x:0, y:0},
-						dimensions: {h:234, w:32},
-						Init: function(game) {
-							game.Behaviours.TwoPoint.register(this, {x:0, y:0}, {x:25, y:40}, 2);
-						}
-						Update: function(game) {
-							game.Behaviours.TwoPoint.update(this);
-						}
-					}
-				],
-				Foregrounds: [
+var SampleWorld = {
+    blocks: {
+        "": {Draw: function(){}},
+        "earth": {
+            sound: {
+                step: "sound-step-earth"
+            },
+            Draw: function(d) {
+                d.drawImage(d.getAsset("texture-earth"), 0, 0);
+            }
+        },
+        "water": {
+            Draw: function(d) {
+                d.drawAnimation()
+            }
+        }
+    },
+    scenes: {
+        level1: {
+            childs: {
+                normal: {
+                    Backgrounds: [
+                        {
+                            position: {x:0, y:0},
+                            dimensions: {h:768, w:1366},
+                            parallax: {x:0.75, y:1},
+                            Update: function(game) {
+                                this.position.x = game.Camera.position.x * this.parallax.x;
+                                this.position.y = game.Camera.position.y * this.parallax.y;
+                            },
+                            Draw: function(d) {
+                                d.drawImage(d.getAsset("background1"), this.position.x, this.position.y);
+                            }
+                        }
+                    ],
+                    Terrain: [
+                        ["earth",""]
+                    ],
+                    Objects: [
+                        {
+                            position: {x:0, y:0},
+                            dimensions: {h:234, w:32},
+                            Init: function(game) {
+                                game.Behaviours.TwoPoint.register(this, {x:0, y:0}, {x:25, y:40}, 2);
+                            }
+                            Update: function(game) {
+                                game.Behaviours.TwoPoint.update(this);
+                            }
+                        }
+                    ],
+                    Foregrounds: [
 
-				],
-				SpawnPoints: [
+                    ],
+                    SpawnPoints: {
+                        spawnPoint1: {x:25, y:567}
+                    },
+                    Music: {
+                        Main: ["mainSong.mp3", "mainSong.ogg"],
+                        awesomeSong: ["song.mp3","song.ogg"]
+                    }
+                },
+                snowy: {
 
-				],
-				Music: {
-
-				}
-			}
-		]
-	}
-]
+                }
+            }
+        }
+    }
+}
