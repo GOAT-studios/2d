26d9e9a4a6ec6caf0dc8323f4eb4066ebd9422de
Tuur Dutoit <tuur.dutoit@live.nl>
2014-07-26 17:46:02 +0200

    Added animation-collection

---
+++ src/plugins/animation-collections.js
@@ -0,0 +1,51 @@
+(function() {
+
+
+
+var Plugin = function() {
+	return AnimationCollection;
+}
+
+var AnimationCollection = function(animations, current) {
+	if(animations instanceof Array) {
+		var obj = {};
+		for(var i = 0, len = animations.length; i < len; i++) {
+			obj[animations[i].name] = animations[i];
+		}
+		this.animations = obj;
+	}
+	else {
+		this.animations = animations;
+	}
+
+	this.current = current || Object.keys(this.animations)[0];
+
+	return this;
+}
+
+AnimationCollection.prototype.name = "animation-collection-basic";
+AnimationCollection.prototype.type = "AnimationCollection";
+
+AnimationCollection.prototype.add = function(animation) {
+	this.animations[animation.name] = animation;
+
+	return this;
+}
+
+AnimationCollection.prototype.remove = function(name) {
+	delete this.animations[name];
+
+	return this;
+}
+
+AnimationCollection.prototype.play = function(name) {
+	this.animations[this.current].stop();
+	this.animations[name].start();
+	this.current = name;
+
+	return this;
+}
+
+
+
+})();
