eaa92e0350f66cc1abe0731626d4e758d55c03cc
Tuur Dutoit <tuur.dutoit@live.nl>
2014-07-26 17:03:06 +0200

    Added plugins array

---
+++ test/assets/test.js
@@ -0,0 +1,4 @@
+// Set up Game
+
+// Include all plugins
+var plugins = ["animations-frame", "assets-dom", "assets-xhr", "behaviour-parallax", "behaviour-two-points", "behaviours", "camera-sidescroller", "colliders-basic", "draw-canvas", "sound-basic", "spirtes-basic", "world-sidescroller"];
