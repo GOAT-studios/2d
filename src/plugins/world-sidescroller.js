428f0d4059b343a9e563647184cbddc0425f86ed
Tuur Dutoit <tuur.dutoit@live.nl>
2014-07-27 20:52:18 +0200

    Updated to world-sidescroller

--- src/plugins/world-sidescroller.js
+++ src/plugins/world-sidescroller.js
@@ -3,6 +3,8 @@
 
 
 var World = function() {
+    this.blocks = {};
+
     return this;
 }
 
@@ -17,19 +19,82 @@ World.prototype.Update = function(game) {
 	
 }
 
+
+
+/**
+ * Scenes
+ */
 World.prototype.load = function(world) {
-    this.blocks = world.blocks;
-    this.scenes = world.scenes;
+    this.world = Game.prototype.Utils.merge({}, world);
 
     return this;
 }
 
-World.prototype.loadScene = function(name, spawnPoint) {
-    var mother = this.scenes[name];
-    var scene = mother[this.game.Save.loadedFile.states[name]];
-    this.currentScene = this.parseScene(scene);
+World.prototype.loadScene = function(name) {
+    var modifiers = this.game.Save.file.modifiers[name] || {};
+    this.currentScene = this.parseScene(this.world[name], modifiers);
+    this.current = name;
 
-    this.game.Player.spawn(this.currentScene.SpawnPoints[spawnPoint]);
+    return this;
+}
+
+
+
+/**
+ * Blocks
+ */
+
+World.prototype.saveBlock = function(name, block) {
+    this.blocks[name] = block;
+
+    return this;
+}
+
+World.prototype.saveBlocks = function(blocks) {
+    Game.prototype.Utils.merge(this, blocks);
+
+    return this;
+}
+
+
+/**
+ * Parsers
+ */
+
+World.prototype.parseScene = function(scene, modifiers) {
+    for(name in scene) {
+        if(name !== "Base" && modifiers[name]) {
+            this.mergeModifier(scene.Base, scene[name]);
+        }
+    }
+
+    var scene = scene.Base;
+}
+
+World.prototype.mergeModifier = function(base, mod) {
+    if(mod.Backgrounds) base.Backgrounds = base.Backgrounds.concat(mod.Backgrounds);
+    if(mod.Objects) base.Objects = base.Objects.concat(mod.Objects);
+    if(mod.Foregrounds) base.Foregrounds = base.Foregrounds.concat(mod.Foregrounds);
+    if(mod.SpawnPoints) base.SpawnPoints = base.SpawnPoints.concat(mod.SpawnPoints);
+    if(mod.Music) base.Music = base.Music.concat(mod.Music);
+
+    if(mod.Terrain) {
+        for(var i = 0, len = mod.Terrain.length; i < len; i++) {
+            var block = mod.Terrain[i];
+            base.Terrain[block.position.x][block.position.y] = block.block;
+        }
+    }
+
+    return this;
+}
+
+
+/**
+ * Other
+ */
+
+World.prototype.getCurrent = function() {
+    return this.currentScene;
 }
 
 
--- test/sampleWorld.js
+++ test/sampleWorld.js
@@ -1,71 +1,53 @@
 var SampleWorld = {
-    blocks: {
-        "": {Draw: function(){}},
-        "earth": {
-            sound: {
-                step: "sound-step-earth"
+    level1: {
+        Base: {
+            Backgrounds: [
+                {
+                    position: {x:0, y:0},
+                    dimensions: {h:768, w:1366},
+                    parallax: {x:0.75, y:1},
+                    Update: function(game) {
+                        this.position.x = game.Camera.position.x * this.parallax.x;
+                        this.position.y = game.Camera.position.y * this.parallax.y;
+                    },
+                    Draw: function(d) {
+                        d.drawImage(d.getAsset("background1"), this.position.x, this.position.y);
+                    }
+                }
+            ],
+            Terrain: [
+                ["earth",""]
+            ],
+            Objects: [
+                {
+                    position: {x:0, y:0},
+                    dimensions: {h:234, w:32},
+                    Init: function(game) {
+                        game.Behaviours.TwoPoint.register(this, {x:0, y:0}, {x:25, y:40}, 2);
+                    }
+                    Update: function(game) {
+                        game.Behaviours.TwoPoint.update(this);
+                    }
+                }
+            ],
+            Foregrounds: [
+
+            ],
+            SpawnPoints: {
+                spawnPoint1: {x:25, y:567}
             },
-            Draw: function(d) {
-                d.drawImage(d.getAsset("texture-earth"), 0, 0);
+            Music: {
+                Main: ["mainSong.mp3", "mainSong.ogg"],
+                awesomeSong: ["song.mp3","song.ogg"]
             }
         },
-        "water": {
-            Draw: function(d) {
-                d.drawAnimation()
-            }
-        }
-    },
-    scenes: {
-        level1: {
-            Base: {
-                Backgrounds: [
-                    {
-                        position: {x:0, y:0},
-                        dimensions: {h:768, w:1366},
-                        parallax: {x:0.75, y:1},
-                        Update: function(game) {
-                            this.position.x = game.Camera.position.x * this.parallax.x;
-                            this.position.y = game.Camera.position.y * this.parallax.y;
-                        },
-                        Draw: function(d) {
-                            d.drawImage(d.getAsset("background1"), this.position.x, this.position.y);
-                        }
-                    }
-                ],
-                Terrain: [
-                    ["earth",""]
-                ],
-                Objects: [
-                    {
-                        position: {x:0, y:0},
-                        dimensions: {h:234, w:32},
-                        Init: function(game) {
-                            game.Behaviours.TwoPoint.register(this, {x:0, y:0}, {x:25, y:40}, 2);
-                        }
-                        Update: function(game) {
-                            game.Behaviours.TwoPoint.update(this);
-                        }
-                    }
-                ],
-                Foregrounds: [
-
-                ],
-                SpawnPoints: {
-                    spawnPoint1: {x:25, y:567}
-                },
-                Music: {
-                    Main: ["mainSong.mp3", "mainSong.ogg"],
-                    awesomeSong: ["song.mp3","song.ogg"]
+        randomBlock: {
+            Terrain: [
+                {
+                    position: {x:2, y:5},
+                    block: "earth"
                 }
-            },
-            randomBlock: {
-                Terrain: [
-                    {
-                        position: {x:2, y:5},
-                        block: "earth"
-                    }
-                ]
-            }
+            ]
         }
     }
 }
