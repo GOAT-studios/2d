aaaeb52afa44d1975babc031d2c6ac78f66cbfb6
Tuur Dutoit <tuur.dutoit@live.nl>
2014-08-05 21:14:07 +0200

    Annotated some code in world-sidescroller

--- src/plugins/world-sidescroller.js
+++ src/plugins/world-sidescroller.js
@@ -23,6 +23,7 @@ World.prototype.Name = "world-sidescroller";
 World.prototype.Type = "World";
 
 World.prototype.Update = function(game) {
+// filter matches anything except 'terrainBuffer'
     var filter = /[^(terrainBuffer)]/i;
 	this.loop(game.Categories, filter, function(type, object) {
         if(object.Update) object.Update(game);
@@ -34,9 +35,12 @@ World.prototype.Update = function(game) {
 }
 
 World.prototype.Draw = function(game) {
-    var blockSize = this.blockSize;
-    var Draw = this.game.Draw;
+    var game = this.game;
+    var Camera = game.Camera.position;
+    var Draw = game.Draw;
     var d = Draw.instance;
+    var blockSize = this.blockSize;
+// filter matches anything except 'terrainBuffer'
     var terrainType = /^terrainBuffer$/i;
     var otherType = /^(backgrounds|foregrounds|objects)$/i;
     var allTypes = /^(backgrounds|foregrounds|objects|terrainBuffer)$/i
@@ -50,8 +54,8 @@ World.prototype.Draw = function(game) {
             object.Draw(d);
         }
         else if(terrainType.test(type) && object.Draw) {
-            Draw.x = i.x;
-            Draw.y = i.y;
+            Draw.x = (i.x * blockSize.width) - Camera.x;
+            Draw.y = (game.height - (i.y * blockSize.height)) + Camera.y;
             Draw.width = blockSize.width;
             Draw.height = blockSize.height;
             object.Draw(d);
@@ -82,6 +86,7 @@ World.prototype.loadScene = function(name) {
     }
 
     var game = this.game;
+// filter matches anything except 'terrainBuffer'
     var filter = /[^(terrainBuffer)]/i;
     this.loop(game.Categories, filter, function(type, object) {
         if(object.Init) object.Init(game);
@@ -114,9 +119,13 @@ World.prototype.saveObjects = function(object) {
  */
 
 World.prototype.parseScene = function(Scene, modifiers) {
+// Copy Base
     var scene = Game.prototype.Utils.merge({}, Scene.Base);
+// Loop through the modifiers
     for(name in Scene) {
+// Check if the modifier is not called Base (that's the base scene) and the modifier should be included
         if(name !== "Base" && modifiers[name]) {
+// Merge the modifier (changes <scene>)
             this.mergeModifier(scene, Scene[name]);
         }
     }
@@ -168,6 +177,7 @@ World.prototype.updateBuffer = function(game) {
 }
 
 World.prototype.loop = function(scene, filter, cb) {
+// If no filter is given, use a filter that matches anything (. matches anything except newlines, \n matches newlines)
     if(typeof filter === "function") {cb = filter; filter = /(.|\n)*/i};
 
     if(filter.test("backgrounds")) {
@@ -226,6 +236,7 @@ World.prototype.loop = function(scene, filter, cb) {
 
 World.prototype.replaceObjects = function(scene) {
     var objects = this.objects;
+// filter matches anything except 'terrainBuffer'
     var filter = /[^(terrainBuffer)]/i;
 
     this.loop(scene, filter, function(type, object) {
