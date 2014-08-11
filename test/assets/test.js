7fe3c02dac24bfac96cabfeaf0f760d944600abf
Tuur Dutoit <tuur.dutoit@live.nl>
2014-08-11 17:54:12 +0200

    Removed Jasmine tests

--- test/assets/test.js
+++
@@ -1,1 +0,0 @@
-var plugins = ["animations-basic","assets-dom","assets-xhr","behaviours-basic","camera-sidescroller","categories-basic","colliders-basic","draw-canvas","sound-basic","sprites-basic","world-sidescroller"];
