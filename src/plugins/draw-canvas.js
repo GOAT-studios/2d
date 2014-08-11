b84c8350d311649ebd2970747543e5532274401b
Tuur Dutoit <tuur.dutoit@live.nl>
2014-08-11 14:38:52 +0200

    Some changes to draw-canvas

--- src/plugins/draw-canvas.js
+++ src/plugins/draw-canvas.js
@@ -2,29 +2,26 @@
 
 
 
-var Draw = function(options) {
-    var draw = this;
+var Draw = function(game, options) {
+    var D = this;
+    this.game = game;
 
 //Configuration
     if(!options) var options = {};
     if(!options.defaultConfig) options.defaultConfig = {};
 
-    var X = this.x = options.x || 0;
-    var Y = this.y = options.y || 0;
-    var width  = this.width  = options.width  || 35;
-    var height = this.height = options.height || 35;
+    this.x = options.x || 0;
+    this.y = options.y || 0;
+    this.width  = options.width  || 35;
+    this.height = options.height || 35;
 
     var domElement = this.domElement = document.createElement("canvas");
     var ctx = this.context = domElement.getContext("2d");
-    var canvasWidth  = this.canvasWidth  = domElement.innerWidth;
-    var canvasHeight = this.canvasHeight = domElement.innerHeight;
-    domElement.addEventListener("resize", function(e) {
-        canvasWidth  = domElement.innerWidth;
-        canvasHeight = domElement.innerHeight;
-    });
-    
-    var defaultConfig = Game.prototype.Utils.merge({}, ctx, ["canvas", "currentPath"]);
-    defaultConfig = this.defaultConfig = Game.prototype.Utils.merge(defaultConfig, options.defaultConfig);
+    var canvasWidth  = this.canvasWidth  = 0;
+    var canvasHeight = this.canvasHeight = 0;
+
+    //Anti-aliasing
+    ctx.translate(0.5, 0.5);
 
 
 
@@ -71,7 +68,7 @@ var Draw = function(options) {
             return this;
         },
         clearRect: function(x, y, w, h) {
-            ctx.clearRect(x+X, y+Y, w, h);
+            ctx.clearRect(x+D.x, y+D.y, w, h);
             return this;
         },
         save: function() {
@@ -85,78 +82,78 @@ var Draw = function(options) {
 
         //Paths
         moveTo: function(x, y) {
-            ctx.moveTo(x+X, y+Y);
+            ctx.moveTo(x+D.x, y+D.y);
             return this;
         },
         lineTo: function(x, y) {
-            ctx.lineTo(x+X, y+Y);
+            ctx.lineTo(x+D.x, y+D.y);
             return this;
         },
         rect: function(x, y, w, h) {
-            ctx.rect(x+X, y+Y, w, h);
+            ctx.rect(x+D.x, y+D.y, w, h);
             return this;
         },
         arc: function(x, y, r, sa, ea, cc) {
-            ctx.arc(x+X, y+Y, r, sa, ea, cc);
+            ctx.arc(x+D.x, y+D.y, r, sa, ea, cc);
             return this;
         },
         arcTo: function(x1, y1, x2, y2, r) {
-            ctx.arcTo(x1+X, y1+Y, x2+X, y2+Y, r);
+            ctx.arcTo(x1+D.x, y1+D.y, x2+D.x, y2+D.y, r);
             return this;
         },
         quadraticCurveTo: function(cpx, cpy, x, y) {
-            ctx.quadraticCurveTo(cpx+X, cpy+Y, x+X, y+Y);
+            ctx.quadraticCurveTo(cpx+D.x, cpy+D.y, x+D.x, y+D.y);
             return this;
         },
         bezierCurveTo: function(cp1x, cp1y, cp2x, cp2y, x, y) {
-            ctx.bezierCurveTo(cp1x+X, cp1y+Y, cp2x+X, cp2y+Y, x+X, y+Y);
+            ctx.bezierCurveTo(cp1x+D.x, cp1y+D.y, cp2x+D.x, cp2y+D.y, x+D.x, y+D.y);
             return this;
         },
 
         //Basic shapes
         strokeRect: function(x, y, w, h) {
-            ctx.strokeRect(x+X, y+Y, w, h);
+            ctx.strokeRect(x+D.x, y+D.y, w, h);
             return this;
         },
         fillRect: function(x, y, w, h) {
-            ctx.fillRect(x+X, y+Y, w, h);
+            ctx.fillRect(x+D.x, y+D.y, w, h);
             return this;
         },
         fullRect: function(x, y, w, h) {
-            ctx.strokeRect(x+X, y+Y, w, h);
-            ctx.fillRect(x+X, y+Y, w, h);
+            ctx.strokeRect(x+D.x, y+D.y, w, h);
+            ctx.fillRect(x+D.x, y+D.y, w, h);
             return this;
         },
         strokeCircle: function(x, y, r) {
             ctx.beginPath();
-            ctx.arc(x+X, y+Y, 0, 2*Math.PI);
+            ctx.arc(x+D.x, y+D.y, 0, 2*Math.PI);
             ctx.stroke();
             return this;
         },
         fillCircle: function(x, y, r) {
             ctx.beginPath();
-            ctx.arc(x+X, y+Y, 0, 2*Math.PI);
+            ctx.arc(x+D.x, y+D.y, 0, 2*Math.PI);
             ctx.fill();
             return this;
         },
         fullCircle: function(x, y, r) {
             ctx.beginPath();
-            ctx.arc(x+X, y+Y, 0, 2*Math.PI);
+            ctx.arc(x+D.x, y+D.y, 0, 2*Math.PI);
             ctx.stroke();
             ctx.fill();
             return this;
         },
         strokeText: function(text, x, y, maxWidth) {
-            ctx.strokeText(text, x+X, y+Y, maxWidth);
+            ctx.strokeText(text, x+D.x, y+D.y, maxWidth);
             return this;
         },
         fillText: function(text, x, y, maxWidth) {
-            ctx.fillText(text, x+X, y+Y, maxWidth);
+            ctx.fillText(text, x+D.x, y+D.y, maxWidth);
             return this;
         },
         fullText: function(text, x, y, maxWidth) {
-            ctx.strokeText(text, x+X, y+Y, maxWidth);
-            ctx.fillText(text, x+X, y+Y, maxWidth);
+            ctx.strokeText(text, x+D.x, y+D.y, maxWidth);
+            ctx.fillText(text, x+D.x, y+D.y, maxWidth);
             return this;
         },
         measureText: function(text) {
@@ -165,7 +162,7 @@ var Draw = function(options) {
 
         //Images
         drawImage: function(image, sx, sy, sw, sh, dx, dy, dw, dh) {
-            ctx.drawImage(image, sx+X, sy+Y, sw, sh, dx?dx+X:undefined, dy?dy+Y:undefined, dw, dh);
+            ctx.drawImage(image, sx+D.x, sy+D.y, sw, sh, dx?dx+D.x:undefined, dy?dy+D.y:undefined, dw, dh);
             return this;
         },
         drawSprite: function(sprite) {
@@ -176,11 +173,11 @@ var Draw = function(options) {
             return this;
         },
         getImageData: function(x, y, w, h) {
-            ctx.getImageData(x+X, y+Y, w, h);
+            ctx.getImageData(x+D.x, y+D.y, w, h);
             return this;
         },
         putImageData: function(imageData, x, y, sx, sy, sw, sh) {
-            ctx.putImageData(imageData, x+X, y+Y, sx?sx+X:undefined, sy?sy+Y:undefined, sw, sh);
+            ctx.putImageData(imageData, x+D.x, y+D.y, sx?sx+D.x:undefined, sy?sy+D.y:undefined, sw, sh);
             return this;
         },
 
@@ -204,21 +201,21 @@ var Draw = function(options) {
 
         //Collisions
         isPointInPath: function(x, y) {
-            return ctx.isPointInPath(x+X, y+Y);
+            return ctx.isPointInPath(x+D.x, y+D.y);
         },
         isPointInStroke: function(x, y) {
-            return ctx.isPointInStroke(x+X, y+Y);
+            return ctx.isPointInStroke(x+D.x, y+D.y);
         },
 
         //Some other functions
         getAsset: function(id) {
-            return draw.game.Assets.get(id);
+            return D.loader.get(id);
         },
         getWidth: function() {
-            return width;
+            return D.width;
         },
         getHeight: function() {
-            return height;
+            return D.height;
         },
         getCanvasWidth: function() {
             return canvasWidth;
@@ -232,20 +229,37 @@ var Draw = function(options) {
     return this;
 }
 
-Draw.prototype.Name = "draw-canvas";
-Draw.prototype.Type = "Draw";
-
-Draw.prototype.Init = function(game) {
-    this.game = game;
-}
-
 Draw.prototype.Update = function(game) {
     this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 }
 
+Draw.prototype.setLoader = function(loader) {
+    this.loader = loader;
+
+    return this;
+}
+
+Draw.prototype.updateSize = function() {
+    this.canvasWidth  = this.domElement.offsetWidth;
+    this.canvasHeight = this.domElement.offsetHeight;
+
+    return this;
+}
 
 
-Game.plugins.push(Draw);
+
+var Plugin = {
+    name: "draw-canvas",
+    id: "core.draw-canvas",
+    path: "Draw",
+    construct: function(game) {
+        return new Draw(game);
+    }
+}
+
+
+
+Game.plugins.push(Plugin);
 
 
 
